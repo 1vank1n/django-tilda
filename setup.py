b62d85cb74a66b727613de5a1444a09c2ae23ce4
Ivan Lukyanets <ivan@il-studio.ru>
2017-12-25 23:37:23 +0300

    add Python 2.7 support
    up to v1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-tilda',
-    version='1.0.2',
+    version='1.0.3',
     author='Ivan Lukyanets',
     author_email='lukyanets.ivan@gmail.com',
     url='https://github.com/1vank1n/django-tilda',

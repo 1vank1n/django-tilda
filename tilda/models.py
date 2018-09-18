aca633cd96471364f3d6e485daca5e0596390f8c
Ivan Lukyanets <ivan@il-studio.ru>
2018-09-18 12:03:45 +0300

    fix translation to lazy method, up to 1.0.7
    Miss unnecessary migration

--- tilda/models.py
+++ tilda/models.py
@@ -2,7 +2,7 @@
 import os
 from django.db import models
 from django.conf import settings
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class TildaPage(models.Model):

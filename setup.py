8f382be855e8ae79b4a493502a65c143b5dbb5e1
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2022-04-11 13:29:18 +0300

    hf: bump version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-tilda',
-    version='1.0.13',
+    version='1.0.14',
     author='Ivan Lukyanets',
     author_email='lukyanets.ivan@gmail.com',
     url='https://github.com/1vank1n/django-tilda',
